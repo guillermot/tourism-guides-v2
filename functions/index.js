3d7f247e8cf6701bd48fa918f456f133f808568b
guillermot <guillermo.tamanaha@gmail.com>
2017-07-16 01:14:15 -0300

    trying to join witjh places

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
 const functions = require('firebase-functions');
-var admin = require('firebase-admin');
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./tourism-guides-firebase-adminsdk-425nr-0181c9d078.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://tourism-guides.firebaseio.com'
+})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -8,17 +15,27 @@ var admin = require('firebase-admin');
 // });
 
 exports.getGuides = functions.https.onRequest((req, resp) => {
-    var serviceAccount = require('./tourism-guides-firebase-adminsdk-425nr-0181c9d078.json');
-
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: 'https://tourism-guides.firebaseio.com'
-    })
 
     const database = admin.database();
 
-    return database.ref('/places').once('value', snap => {
-        resp.status(200).send(snap.val());
+    return database.ref('/guides').once('value', snap => {
+        const guides = snap.val();
+
+        for (var key in guides) {
+            if (guides.hasOwnProperty(key)) {
+                const guide = guides[key];
+                if (guide.places && guide.places.length > 0) {
+                    for (var i = 0; i < guide.places.length; i++) {
+                        const placeKey = guide.places[i];
+                        const placeRef = `/places/${placeKey}`;
+                        console.info('placeRef: ' + placeRef);
+                        database.ref(placeRef).once('value', placeSnap => guide.places[i] = placeSnap.val());
+                    }
+                }
+            }
+        }
+
+        resp.status(200).send(guides);
     });
 });
 
