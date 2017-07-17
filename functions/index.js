d8b644f00eafd815583ff6e04ba40175164b81a8
guillermot <guillermo.tamanaha@gmail.com>
2017-07-16 22:34:29 -0300

    api done

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express');
 
 const serviceAccount = require('./tourism-guides-firebase-adminsdk-425nr-0181c9d078.json');
+const app = express();
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -14,28 +16,106 @@ admin.initializeApp({
 //  response.send("Hello from Firebase!");
 // });
 
-exports.getGuides = functions.https.onRequest((req, resp) => {
-
+app.get('/api/guides', (req, resp) => {
     const database = admin.database();
 
-    return database.ref('/guides').once('value', snap => {
-        const guides = snap.val();
+    return database.ref('/guides')
+        .once('value')
+        .then(snap => resp.status(200).send(snap.val()));
+});
 
-        for (var key in guides) {
-            if (guides.hasOwnProperty(key)) {
-                const guide = guides[key];
-                if (guide.places && guide.places.length > 0) {
-                    for (var i = 0; i < guide.places.length; i++) {
-                        const placeKey = guide.places[i];
-                        const placeRef = `/places/${placeKey}`;
-                        console.info('placeRef: ' + placeRef);
-                        database.ref(placeRef).once('value', placeSnap => guide.places[i] = placeSnap.val());
+app.get('/api/place/:id', (req, resp) => {
+    const database = admin.database();
+
+    const id = req.params.id;
+
+    if (id) {
+        return database.ref(`/places/${id}`)
+            .once('value')
+            .then(snap => resp.status(200).send(snap.val()));
+    }
+
+    resp.status(404);
+});
+
+app.get('/api/fullGuides', (req, resp) => {
+    const database = admin.database();
+    let guides = undefined;
+
+    return database.ref('/guides').once('value')
+        .then(snap => {
+            let promises = [];
+            guides = snap.val();
+            for (var key in guides) {
+                if (guides.hasOwnProperty(key)) {
+                    const guide = guides[key];
+
+                    if (guide.places) {
+                        for (var i = 0; i < guide.places.length; i++) {
+                            const placeIndex = i;
+                            const placePromise = database.ref(`/places/${guide.places[i]}`).once('value', placeSnap => {
+                                return placeSnap.val();
+                            }).then(place => {
+                                guide.places[placeIndex] = place.val();
+                            });
+
+                            promises.push(placePromise);
+                        }
                     }
                 }
             }
-        }
 
-        resp.status(200).send(guides);
-    });
+            return Promise.all(promises);
+        }).then(() => {
+            resp.status(200).send(guides);
+        });
 });
+
+exports.api = functions.https.onRequest(app);
+
+// exports.guides = functions.https.onRequest((req, resp) => {
+
+//     const database = admin.database();
+
+//     return database.ref('/guides').once('value')
+//         .then(snap => {
+//             const guides = snap.val();
+
+//             resp.status(200).send(guides);
+//         });
+// });
+
+// exports.fullGuides = functions.https.onRequest((req, resp) => {
+
+//     const database = admin.database();
+//     let guides = undefined;
+
+//     return database.ref('/guides').once('value')
+//         .then(snap => {
+//             let promises = [];
+//             guides = snap.val();
+//             for (var key in guides) {
+//                 if (guides.hasOwnProperty(key)) {
+//                     const guide = guides[key];
+
+//                     if (guide.places) {
+//                         for (var i = 0; i < guide.places.length; i++) {
+//                             const placeIndex = i;
+//                             const placePromise = database.ref(`/places/${guide.places[i]}`).once('value', placeSnap => {
+//                                 return placeSnap.val();
+//                             }).then(place => {
+//                                 guide.places[placeIndex] = place.val();
+//                             });
+
+//                             promises.push(placePromise);
+//                         }
+//                     }
+//                 }
+//             }
+
+//             return Promise.all(promises);
+//         }).then(() => {
+//             resp.status(200).send(guides);
+//         });
+// });
 
